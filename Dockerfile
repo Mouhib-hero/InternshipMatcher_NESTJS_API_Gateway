b853b66c96773683880ff63d4d21b9be17656331
mpoyd <m.jerbi2@gmail.com>
2023-04-13 01:54:19 +0100

    Added user to DockerFile

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,8 @@ ADD . /var/www/gateway
 RUN npm install -g npm@8.15.0
 RUN npm i -g @nestjs/cli
 RUN npm install
-RUN npm i --save @nestjs/config
-RUN npm i @nestjs/microservices @grpc/grpc-js @grpc/proto-loader
-RUN npm i -D @types/node ts-proto
-RUN npm run proto:install 
-#RUN npm run proto:offer
+RUN npm run proto:install
+RUN adduser -D myuser
+RUN chown -R myuser: /var/www/gateway
+USER myuser 
 ENTRYPOINT nest start
-#RUN npm run build
-#CMD ["node", "dist/main.ts"]
--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -7,10 +7,7 @@ import {
 } from '@nestjs/terminus';
 import { PrometheusService } from '../prometheus/prometheus.service';
 import { HealthIndicator } from './interfaces/health-indicator.interface';
-import { AppSvcConnectivityHealthIndicator } from './models/app-svc-connectivity-health.indicator';
-import { AuthSvcConnectivityHealthIndicator } from './models/auth-svc-connectivity-health.indicator';
 import { NestjsHealthIndicator } from './models/nestjs-health.indicator';
-import { OfferSvcConnectivityHealthIndicator } from './models/offer-svc-connectivity-health.indicator';
 
 @Injectable()
 export class HealthService {
@@ -27,9 +24,6 @@ export class HealthService {
         'http://localhost:3001',
         this.promClientService
       ),
-      //new AppSvcConnectivityHealthIndicator(this.http,process.env.HOSTAPP, this.promClientService),
-      //new AuthSvcConnectivityHealthIndicator(this.http,process.env.HOSTAUTH, this.promClientService),
-      //new OfferSvcConnectivityHealthIndicator(this.http,process.env.HOSTOFFER, this.promClientService),
     ];
   }
 
